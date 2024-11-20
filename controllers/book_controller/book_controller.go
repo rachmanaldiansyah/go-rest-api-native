7da17b8b88a46a68b154b0a0303a497b2263cce1
RACHMAN21 <rachmanaldiansyah03@gmail.com>
2024-11-20 17:48:54 +0700

    feature-books: detail & update books api

--- controllers/book_controller/book_controller.go
+++ controllers/book_controller/book_controller.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 
 	"go-api-native/config"
@@ -32,6 +34,38 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.Response(w, 200, "List Books", booksResponse)
+}
+
+// Detail will return the detail of a book by ID
+// @Summary Get book by ID
+// @Description Get book by ID
+// @ID get-book-by-id
+// @Tags books
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Book ID"
+// @Success 200 {object} helpers.Response
+// @Failure 404 {object} helpers.Response
+// @Failure 500 {object} helpers.Response
+// @Router /books/{id}/detail [get]
+func Detail(w http.ResponseWriter, r *http.Request) {
+	idParams := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParams)
+
+	var books models.Book
+	var booksResponse models.BookResponse
+
+	if err := config.DB.Joins("Author").First(&books, id).First(&booksResponse, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helpers.Response(w, 404, "Book not found", nil)
+			return
+		}
+
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helpers.Response(w, 200, "Detail Book", booksResponse)
 }
 
 // Create will create a new book in the database
@@ -74,4 +108,63 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.Response(w, 201, "Create book successfully", nil)
+}
+
+// Update will update an existing book in the database based on the provided ID.
+// @Summary Update book
+// @Description Update book
+// @ID update-book
+// @Tags books
+// @Accept json
+// @Produce json
+// @Param id path int true "Book ID"
+// @Param book body models.Book true "Book data"
+// @Success 200 {object} helpers.Response
+// @Failure 404 {object} helpers.Response
+// @Failure 500 {object} helpers.Response
+// @Router /books/{id}/update [put]
+func Update(w http.ResponseWriter, r *http.Request) {
+	idParams := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParams)
+
+	var books models.Book
+	var booksResponse models.BookResponse
+
+	if err := config.DB.First(&books, id).First(&booksResponse, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helpers.Response(w, 404, "Book not found", nil)
+			return
+		}
+
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	var booksPayload models.Book
+	if err := json.NewDecoder(r.Body).Decode(&booksPayload); err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	defer r.Body.Close()
+
+	var author models.Author
+	if booksPayload.AuthorID != 0 {
+		if err := config.DB.First(&author, booksPayload.AuthorID).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				helpers.Response(w, 404, "Author not found", nil)
+				return
+			}
+
+			helpers.Response(w, 500, err.Error(), nil)
+			return
+		}
+	}
+
+	if err := config.DB.Where("id = ?", id).Updates(&booksPayload).Error; err != nil {
+		helpers.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helpers.Response(w, 200, "Update book successfully", nil)
 }
--- routes/book.go
+++ routes/book.go
@@ -11,7 +11,7 @@ func BookRoutes(r *mux.Router) {
 
 	router.HandleFunc("", book_controller.Index).Methods("GET")
 	router.HandleFunc("", book_controller.Create).Methods("POST")
-	// router.HandleFunc("/{id}/detail", book_controller.Detail).Methods("GET")
-	// router.HandleFunc("/{id}/update", book_controller.Update).Methods("PUT")
+	router.HandleFunc("/{id}/detail", book_controller.Detail).Methods("GET")
+	router.HandleFunc("/{id}/update", book_controller.Update).Methods("PUT")
 	// router.HandleFunc("/{id}/delete", book_controller.Delete).Methods("DELETE")
 }
